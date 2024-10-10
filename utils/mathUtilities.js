bfcf67d38576dbfcb5768f5e72764b730feb8611
ahllyy <amanda.halley@keyin.com>
2024-10-10 17:47:54 -0230

    add some logic for isCorrectAnswer function (incomplete)

--- utils/mathUtilities.js
+++ utils/mathUtilities.js
@@ -23,13 +23,45 @@ console.log(getQuestion());
 /**
  * Parses the provided question and gets whether or not the provided answer is correct
  * 
- * @param {*} question The question being answered
- * @param {*} answer The potential answer
+ * @param {string} question The question being answered
+ * @param {number} answer The potential answer
  * @returns {boolean} True if the answer was correct, false otherwise.
  */
 function isCorrectAnswer(question, answer) {
-    return false;
+    //split question
+    const [a, operator, b] = question.split();
+     
+    //convert numbers to integer from string
+    const num1 = parseInt(a, 10);
+    const num2 = parseInt(b, 10);
+
+    // get value of the result of the question based on which operator is used
+    let correctAnswer;
+    switch(operator) {
+        case '*':
+            correctAnswer = num1 + num2;
+            break;
+        case '/':
+            correctAnswer = num1 / num2;
+            break;
+        case '-':
+            correctAnswer = num1 - num2;
+            break;
+        case '+':
+        correctAnswer = num1 + num2;
+        break;
+        
+        default:
+            return false;
+    }
+
+    return correctAnswer === answer;
 }
+// // Example usage of isCorrectAnswer
+// const question = getQuestion();
+// console.log(question); // To see the generated question
+// console.log(isCorrectAnswer(question,3)); // Replace 42 with a sample answer for testing
+
 
 module.exports = {
     getQuestion,
