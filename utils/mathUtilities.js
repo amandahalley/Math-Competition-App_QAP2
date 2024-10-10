d0a872debc8f271b84f8e0980674f61a8128e6a0
ahllyy <amanda.halley@keyin.com>
2024-10-10 15:57:31 -0230

    create function to generate random math question

--- utils/mathUtilities.js
+++ utils/mathUtilities.js
@@ -1,11 +1,24 @@
 /**
  * Gets a random multiplication, division, subtraction or addition question
  * 
- * @returns {} The randomly generated math question
+ * @returns {string} The randomly generated math question
  */
 function getQuestion() {
+    //define possible opperators
+    const operators= ['*', '/', '-', '+'];
 
+    //randomly choose one of the defined operators
+    const randomOperator = operators[Math.floor(Math.random() * operators.length)];
+
+
+    //randomly generate a number for each variable between 1 & 10
+    var a = Math.floor(Math.random() * 10) + 1;
+    var b = Math.floor(Math.random() * 10) + 1;
+
+    return `${a} ${randomOperator} ${b}`;
 }
+
+console.log(getQuestion());
 
 /**
  * Parses the provided question and gets whether or not the provided answer is correct
