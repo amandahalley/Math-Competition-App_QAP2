82a17ef39997932a828332b7172ebb70175aeea6
ahllyy <amanda.halley@keyin.com>
2024-10-14 15:51:01 -0230

    add two tests for getQuestion

--- __tests__/utils/mathUtilities.test.js
+++ __tests__/utils/mathUtilities.test.js
@@ -1,8 +1,24 @@
 const { isCorrectAnswer, getQuestion } = require("../../utils/mathUtilities");
 
 describe("Tests for getQuestion", () => {
+    test("Returns a question in the correct format (a, operator, b)", () => { 
+        const question = getQuestion();
+        expect(typeof question).toBe("string"); // check for string
+        expect(question.split(' ').length).toBe(3); // checks for correct format
+    }); 
+
+    test("Uses a valid operator (+, -, *, /)", () => {
+        const question = getQuestion();
+        const operator = question.split(' ')[1];
+        expect(['+', '-', '*', '/']).toContain(operator);
+    });
+
 
 });
+
+
+
+
 
 describe("Tests for isCorrectAnswer", () => {
     
