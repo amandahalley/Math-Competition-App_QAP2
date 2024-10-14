b96252892a9aa196a2ae6b5e61240be3e2ce4664
ahllyy <amanda.halley@keyin.com>
2024-10-14 15:59:47 -0230

    test for detecting a true or false answer

--- __tests__/utils/mathUtilities.test.js
+++ __tests__/utils/mathUtilities.test.js
@@ -3,23 +3,34 @@ const { isCorrectAnswer, getQuestion } = require("../../utils/mathUtilities");
 describe("Tests for getQuestion", () => {
     test("Returns a question in the correct format (a, operator, b)", () => { 
         const question = getQuestion();
-        expect(typeof question).toBe("string"); // check for string
-        expect(question.split(' ').length).toBe(3); // checks for correct format
+        expect(typeof question).toBe("string"); //check for string
+        expect(question.split(' ').length).toBe(3); //checks for correct format
     }); 
 
     test("Uses a valid operator (+, -, *, /)", () => {
         const question = getQuestion();
-        const operator = question.split(' ')[1];
-        expect(['+', '-', '*', '/']).toContain(operator);
+        const operator = question.split(' ')[1]; //gets operator
+        expect(['+', '-', '*', '/']).toContain(operator); //check that operator is valid
     });
 
-
+    //possibly add testing for generating a random question
 });
 
 
 
-
-
 describe("Tests for isCorrectAnswer", () => {
-    
+    test("Detects a correct answer and gives back true", () => {
+        const question = "10 * 5";
+        const answer = 50;
+        const result = isCorrectAnswer(question, answer);
+        expect(result).toBe(true);
+    });
+
+    test("detect a false answer is given and gives back false", () => {
+        const question = "10 * 5";
+        const answer = 20
+        const result = isCorrectAnswer(question, answer);
+        expect(result).toBe(false);
+    });
+
 });
